6216a94329b6f22144d74ad3e0ff7ebb38c5991c
flycool <flycoolcool@163.com>
2024-12-27 18:15:49 +0800

    add template gallary

---
+++ src/constants/template.ts
@@ -0,0 +1,37 @@
+export const templates = [
+  {
+    id: "blank",
+    label: "Blank Document",
+    imageUrl: "/logo.svg",
+  },
+  {
+    id: "software-propasal",
+    label: "Software development proposal",
+    imageUrl: "/logo.svg",
+  },
+  {
+    id: "project-propasal",
+    label: "Project proposal",
+    imageUrl: "/logo.svg",
+  },
+  {
+    id: "business-letter",
+    label: "Business letter",
+    imageUrl: "/logo.svg",
+  },
+  {
+    id: "resume",
+    label: "Resume",
+    imageUrl: "/logo.svg",
+  },
+  {
+    id: "cover-letter",
+    label: "Cover letter",
+    imageUrl: "/logo.svg",
+  },
+  {
+    id: "letter",
+    label: "Letter",
+    imageUrl: "/logo.svg",
+  },
+];
