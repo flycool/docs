73381d420bfc1560e645dcb932406a45c94f0fe1
flycool <flycoolcool@163.com>
2024-12-27 16:15:00 +0800

    add home page, search input

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Link from "next/link";
-
-export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center">
-      <Link href="/documents/123">document here</Link>
-    </div>
-  );
-}
