60fdf9a81286c8213c8a6000dcf3ff1bd542f20c
flycool <flycoolcool@163.com>
2024-12-28 17:57:52 +0800

    add search function

--- convex/documents.ts
+++ convex/documents.ts
@@ -23,9 +23,29 @@ export const create = mutation({
 });
 
 export const get = query({
-  args: { paginationOpts: paginationOptsValidator },
+  args: {
+    paginationOpts: paginationOptsValidator,
+    search: v.optional(v.string()),
+  },
   handler: async (ctx, args) => {
-    return await ctx.db.query("documents").paginate(args.paginationOpts);
+    const user = await ctx.auth.getUserIdentity();
+    if (!user) {
+      throw new ConvexError("Unauthorized");
+    }
+
+    if (args.search) {
+      return await ctx.db
+        .query("documents")
+        .withSearchIndex("search_title", (q) =>
+          q.search("title", args.search!).eq("ownerId", user.subject)
+        )
+        .paginate(args.paginationOpts);
+    }
+
+    return await ctx.db
+      .query("documents")
+      .withIndex("by_owner_id", (q) => q.eq("ownerId", user.subject))
+      .paginate(args.paginationOpts);
   },
 });
 
