4729b610ce24140b58c8fbc6c803a09badcd20cf
flycool <flycoolcool@163.com>
2024-12-29 19:59:19 +0800

    fix magic number

--- src/app/documents/[documentId]/room.tsx
+++ src/app/documents/[documentId]/room.tsx
@@ -11,6 +11,7 @@ import { FullscreenLoader } from "@/components/fullscreen-loader";
 import { toast } from "sonner";
 import { getUsers, getDocuments } from "./actions";
 import { Id } from "../../../../convex/_generated/dataModel";
+import { LEFT_MARGIN_DEFAULT, RIGHT_MARGIN_DEFAULT } from "@/constants/margins";
 
 type User = { id: string; name: string; avatar: string };
 
@@ -72,7 +73,7 @@ export function Room({ children }: { children: ReactNode }) {
     >
       <RoomProvider
         id={params.documentId as string}
-        initialStorage={{ leftMargin: 56, rightMargin: 56 }}
+        initialStorage={{ leftMargin: LEFT_MARGIN_DEFAULT, rightMargin: RIGHT_MARGIN_DEFAULT }}
       >
         <ClientSideSuspense
           fallback={<FullscreenLoader label="Room loading..." />}
