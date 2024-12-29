570bfd56db7f9c4faae2d501e20cd556ab9228b2
flycool <flycoolcool@163.com>
2024-12-29 19:15:19 +0800

    add status of update document input

--- src/app/documents/[documentId]/document-input.tsx
+++ src/app/documents/[documentId]/document-input.tsx
@@ -1,10 +1,12 @@
-import { BsCloudCheck } from "react-icons/bs";
+import { BsCloudCheck, BsCloudSlash } from "react-icons/bs";
 import { Id } from "../../../../convex/_generated/dataModel";
 import React, { useRef, useState } from "react";
 import { useMutation } from "convex/react";
 import { api } from "../../../../convex/_generated/api";
 import { useDebounce } from "@/hooks/use-debounce";
 import { toast } from "sonner";
+import { useStatus } from "@liveblocks/react";
+import { LoaderIcon } from "lucide-react";
 
 interface DocumentInputProps {
   title: string;
@@ -12,6 +14,8 @@ interface DocumentInputProps {
 }
 
 export default function DocumentInput({ title, id }: DocumentInputProps) {
+  const status = useStatus();
+
   const [value, setValue] = useState(title);
   const [isError, setIsError] = useState(false);
   const [isPending, setIsPending] = useState(false);
@@ -50,6 +54,10 @@ export default function DocumentInput({ title, id }: DocumentInputProps) {
       .finally(() => setIsPending(false));
   };
 
+  const showLoader =
+    isPending || status === "connecting" || status === "reconnecting";
+  const showError = status === "disconnected";
+
   return (
     <div className="flex items-center gap-2">
       {isEditing ? (
@@ -79,7 +87,11 @@ export default function DocumentInput({ title, id }: DocumentInputProps) {
         </span>
       )}
 
-      <BsCloudCheck />
+      {showError && <BsCloudSlash className="size-4" />}
+      {!showError && !showLoader && <BsCloudCheck className="size-4" />}
+      {showLoader && (
+        <LoaderIcon className="size-4 animate-spin text-muted-foreground" />
+      )}
     </div>
   );
 }
